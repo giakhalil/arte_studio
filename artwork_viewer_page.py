744500967228af72a1cacab163a189f75f969f6c
giakhalil <gia.khalil@stud.uniroma3.it>
2025-11-09 13:58:09 +0100

    update css

--- artwork_viewer_page.py
+++ artwork_viewer_page.py
@@ -50,7 +50,7 @@ def render():
     
     st.markdown("""
     <div class="warning-box">
-        <h4>Istruzioni importanti</h4>
+        <h4 style="color: #856404;">Istruzioni importanti</h4>
         <ul>
             <li>Leggi attentamente la descrizione e osserva l'opera</li>
             <li><strong>Non prendere appunti</strong></li>
